f53251bd029aa9d569d1db8bf099a0043c460756
Manali Ghare <manalighare19@gmail.com>
2019-11-14 22:37:57 -0500

    change in login api

--- routes/auth.js
+++ routes/auth.js
@@ -65,7 +65,7 @@ router.post('/login', async (req,res) => {
 
     //create and assign a token
     const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
-    res.header('auth-token', token).send(token);
+    res.header('auth-token', token).send({"token": token});
 });
 
 module.exports = router;
--- routes/placesRoute.js
+++ routes/placesRoute.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const mongoose = require('mongoose');
 const Place = require('../model/Place');
 
+//Get places with city name
 router.get('/', async (req,res) => {
     const places = await Place.find({"city":req.body.city});    
     res.send(places);
