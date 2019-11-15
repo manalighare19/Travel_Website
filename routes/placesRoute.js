a5b979d088822911be00862d77e91d528ef51c20
Manali Ghare <manalighare19@gmail.com>
2019-11-15 01:17:48 -0500

    change in places api

--- routes/placesRoute.js
+++ routes/placesRoute.js
@@ -4,13 +4,13 @@ const Place = require('../model/Place');
 const City = require('../model/City');
 
 //Get places with city name
-router.get('/getplaces', async (req,res) => {
+router.post('/getplaces', async (req,res) => {
     const places = await Place.find({"city":req.body.city});    
     res.send(places);
 });
 
 //Get city
-router.get('/getcity', async (req,res) => {
+router.post('/getcity', async (req,res) => {
    
     const cityExist =  await City.findOne({name: req.body.name}); 
     if(cityExist) {
